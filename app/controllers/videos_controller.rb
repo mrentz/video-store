183ae167e4895653c0c4c9b63ebc218179a4e113
Matthew <mrentz@mail.com>
2018-01-09 12:40:38 +1030

    starting a crud for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -10,10 +10,12 @@ class VideosController < ApplicationController
   end
 
   def new
-    @video = Video.new
+#    @video = Video.new
   end
 
   def create
+  title = params[:title]
+  imdbid = params[:imbdid]
     @video = Video.new params[:video]
     if @video.save
       flash[:notice] = "#{@video.title} saved."
