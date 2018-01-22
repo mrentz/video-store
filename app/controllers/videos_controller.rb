747f999a8d278f09dd0f88d588cbee16b858ddf8
Matthew <mrentz@mail.com>
2018-01-22 20:31:44 +1030

    refined sitewide search

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,7 +12,8 @@ class VideosController < ApplicationController
   end
   
   def index
-    if params[:commit] == "site search"
+    case params[:commit]
+    when "site search"
       @videos = Video.where('title ~* ?',
                             "#{params[:search_string]}").paginate(page: params[:page],
                                                                   per_page: 5)
@@ -20,40 +21,41 @@ class VideosController < ApplicationController
         flash[:notice] = "Nothing relating to #{params[:search_string]} can be found."
         redirect_to :action => 'index'
       end
+    when "Search"
+      title = movieData(params[:title])    
+      @movie = saved_locally(title[:title])
+      if @movie.blank? == true
+        @movie = title
+        render :details
       else
+        redirect_to video_path(@movie.id)
+      end
+    else
       @videos = Video.all.paginate(page: params[:page], per_page: 5)
     end
   end
   
   def create
-    @movie = saved_locally(params[:title])
-    if @movie.blank? == true && params[:commit] == "Search"
-      @movie = movieData(params[:title])    
-      render :details
-    elsif params[:commit] == "Save"
-      @movie = Video.new movieData(params[:title])
-      if @movie.save
-        flash[:notice] = "#{@movie.title} saved."
-        redirect_to videos_path(@movie)
-      end
-    else
-      redirect_to video_path(@movie.id)
+    @movie = Video.new movieData(params[:title])
+    if @movie.save
+      flash[:notice] = "#{@movie.title} saved."
+      redirect_to videos_path(@movie)
     end
   end
-  
-   def destroy
-     video = Video.find(params[:id])
-     video.destroy
-     flash[:notice] = "#{video.title} deleted."
-     redirect_to :action => 'index'
-   end
   
   def show
     @movie = Video.find(params[:id])
   end
   
+  def destroy
+    video = Video.find(params[:id])
+    video.destroy
+    flash[:notice] = "#{video.title} deleted."
+    redirect_to :action => 'index'
+  end
+  
   def details
-
+    
   end
   
 end
