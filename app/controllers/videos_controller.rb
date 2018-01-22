8cf8a0a4d92e88bd1ca943757071353d7a4946ab
Matthew <mrentz@mail.com>
2018-01-22 11:22:05 +1030

    adding refined sitewide search

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,12 +3,8 @@ require 'stars'
 
 class VideosController < ApplicationController
 
-  def saved(title_search)
-    @movie = Video.where("title ~* ?", "#{title_search}").first
-  end
-
   def saved_locally(title_search)
-    @movie = Video.find_by(title: "#{title_search}")
+    @movie = Video.where('title = ?', "#{title_search.titleize}").first
   end
   
   def new
@@ -17,16 +13,13 @@ class VideosController < ApplicationController
   
   def index
     if params[:commit] == "site search"
-      local_movies = saved(params[:search_string])
-      if local_movies.blank? == true
-        flash[:notice] = "Nothing relating to #{params[:search_string]} can be found."
-        redirect_to :action => 'index'
-      end
       @videos = Video.where('title ~* ?',
                             "#{params[:search_string]}").paginate(page: params[:page],
                                                                   per_page: 5)
-      puts ">>>>>"
- puts ">>>>>>>>>>>>>>> #{params[:search_string]}"
+      if @videos.blank? == true
+        flash[:notice] = "Nothing relating to #{params[:search_string]} can be found."
+        redirect_to :action => 'index'
+      end
       else
       @videos = Video.all.paginate(page: params[:page], per_page: 5)
     end
@@ -44,7 +37,7 @@ class VideosController < ApplicationController
         redirect_to videos_path(@movie)
       end
     else
-      redirect_to video_path(@movie)
+      redirect_to video_path(@movie.id)
     end
   end
   
