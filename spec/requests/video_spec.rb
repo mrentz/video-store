3232488f4de606cbceec452ab6907e1334ba18b8
Your Name <you@example.com>
2018-01-25 15:11:24 +1030

    detailed Capybara request specs in progress

--- spec/requests/video_spec.rb
+++ spec/requests/video_spec.rb
@@ -8,7 +8,7 @@ describe "Visiting the video index page" do
   
   it { is_expected.to have_selector('h1', :text => 'Matthews List of Videos') }
 
-  it 'should search and save movies' do
+  it 'should search and save and delete movies' do
     expect(page).to have_selector('h2', :text => 'Search All Movies')
     fill_in 'title', with: 'Content'
     click_button "Search"
@@ -32,5 +32,29 @@ end
   it 'has working flash messages' do
   end
     
+  it "should have local site title keyphrase search" do
+    fill_in 'title', with: 'Content'
+    click_button "Search"
+    expect(page).to have_content "Content"
+    click_button "Save"
+    click_link "Home"
+    fill_in "Keyword Search", with: "Conte"
+    click_button "Site Search"
+    expect(page).to have_content "Content"
+    expect(page).to have_selector('h1', :text => 'Matthews List of Videos') 
+    expect(page).to have_selector('h2', :text => 'Search All Movies')
+    click_link "Content"
+    expect(page).to have_css('a[data-confirm="Do you really want to delete this video?"]')
+    click_link "Delete"
+  end
+
+  it "site search should filter for actors" do
+  end
+
+  it "site search should filter for genre" do
+  end
+
+  it "site search should filter for both actors and genre" do
+  end
 
 end
