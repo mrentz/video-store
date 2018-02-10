4ce623ec49e67884d9a105e851fc630486bcfd01
Matthew <mrentz@mail.com>
2018-02-10 16:19:41 +1030

    start building video model specs

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -23,4 +23,29 @@ describe Video do
     expect(video2).not_to be_valid
   end
 
-end
+  describe ".search" do
+    
+    video1 = FactoryBot.create(:video)
+    video1.title = "title1"
+    video1.actors = "actor1"
+    video1.save
+    video2 = Video.create(title: "title2", actors: "actor2")
+    video3 = Video.create(title: "title3", actors: "actor3")
+    video4 = Video.create(title: "title4", actors: "actor4")
+    video5 = Video.create(title: "title5", actors: "actor5")
+    video6 = Video.create(title: "title6", actors: "actor6")
+    
+    it "should return full list upon nil search fields" do
+      videos = Video.search(nil, [:theme, :actors])
+      expect(videos).to match_array(Video.all)
+    end
+
+    it "should return videos by title when no fields checked" do
+      i = rand(1..6)
+      videos = Video.search("title#{i}", [])
+      expect(videos).to match_array(Video.find_by title: "title#{i}")
+    end
+    
+    end
+  
+  end
