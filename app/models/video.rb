45e58b1df0726b94b2ee41531224c4b559571e24
Matthew <mrentz@mail.com>
2018-02-11 23:46:29 +1030

    refactored video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,20 +1,20 @@
 class Video < ApplicationRecord
+
   validates_uniqueness_of :title, :case_sensitive => false
 
   def self.search(search_string, search_fields)
-    results = []
+
     return all if search_string.blank?
     if search_fields.blank?
       videos = Video.where("title ~* ?", search_string)
     else
+      videos = Video.where("#{search_fields.pop} ~* ?", search_string)
       search_fields.each do |field|
-        results << Video.where("#{field} ~* ?", search_string)
+        videos = videos.or(Video.where("#{field} ~* ?", search_string))
       end
-      videos = "videos = results.first"
-      for i in 1..results.size-1
-        videos << ".or(results[#{i}])"
-      end
-      return eval videos
+      return videos
     end
+    
   end
+  
 end
